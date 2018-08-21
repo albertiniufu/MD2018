7d4d88a519e72a08c31d2808c47bbca6c6f5c189
Diogo Canut <canut@wcore.com.br>
2018-08-21 15:45:17 -0300

    up

--- dados/download_dados.R
+++ dados/download_dados.R
@@ -5,7 +5,7 @@
 ##################
 
 URL_DATAFILE="http://homolog-dados.mma.gov.br/pt_BR/dataset/ffd9ab35-5719-4ec1-8d13-ae8f738bebc2/resource/ce81bb79-1f8d-430d-b164-455b0e022382/download/pf_certificacao_pefc_2005-2017.csv"
-FILENAME <- "dados/pf_certificacao_pefc_2005-2017.csv"
+FILENAME <- "/home/knut/projetos/MD2018/dados/pf_certificacao_pefc_2005-2017.csv"
 if (file.exists(FILENAME) == FALSE) {
   download.file(URL_DATAFILE, destfile=FILENAME)
 }
