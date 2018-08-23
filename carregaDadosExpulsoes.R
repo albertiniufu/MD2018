4402529ca2fd22123bcea965fe4a45111028f280
Marcelo Kriiger <marcelokcs@hotmail.com>
2018-08-23 20:57:48 -0300

    Atualizações de arquivos.

--- carregaDadosExpulsoes.R
+++ carregaDadosExpulsoes.R
@@ -1,7 +1,9 @@
 library(dplyr)
 
 
-expulsoes <- read.csv("home/marcelokriiger/MD2018/Expulsoes.csv", to.data.frame = TRUE, header = TRUE, sep = ";")
-expulsoes <- select(expulsoes, -c ("MATRICULA", "ORGAO DE LOTACAO", "UF LOTACAO", "NUMERO DE PORTARIA DE PUNICAO", "PAGINA", "SECAO", "NUMERO DO PROCESSO ADMINISTRATIVO"))
+expulsoes <- read.csv("/home/marcelokriiger/MD2018/dados/Expulsoes.csv", header = TRUE, sep = ";")
+expulsoes <- select(expulsoes, -c ("MATRICULA", "ORGAO.DE.LOTACAO","UF.LOTACAO","NUMERO.DE.PORTARIA.DE.PUNICAO", "PAGINA", "SECAO", "NUMERO.DO.PROCESSO.ADMINISTRATIVO"))
+expulsoes$CARGO.EFETIVO <- as.character(expulsoes$CARGO.EFETIVO)
 glimpse(expulsoes)
+
 
