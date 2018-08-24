bdbf6734b4a33414cd5fe33ee1525fc3eac8cb09
GudNunes <you@example.com>
2018-08-24 12:00:48 -0300

    .

--- dados/carregadadosimp.R
+++ dados/carregadadosimp.R
@@ -2,10 +2,13 @@ library(foreign)
 library(dplyr)
 
 impeachment <- read.spss("/home/gudnunes/Área de Trabalho/Mineração de dados/MD2018/dados/bd_vot_impeachment.sav", to.data.frame = TRUE, header = TRUE, sep= ";")
-impeachment <- select(impeachment, -c("sit_cand_elei", "colig_elei", "votacao", "cod_tse_mun", "total_votos_mun", "perc_vot_mun", "hh", "migracao", "data_migr", "camissao", "indicacao_comiss", "condicao_comiss", "vota_comiss"))
+impeachment <- select(impeachment, -c("gastos_camp", "sit_cand_elei", "colig_elei", "votacao", "cod_tse_mun", "total_votos_mun", "perc_vot_mun", "hh", "migracao", "data_migr", "camissao", "indicacao_comiss", "condicao_comiss", "vota_comiss"))
 impeachment$sexo <- as.factor(impeachment$sexo)
 impeachment$indica_bancad <- as.factor(impeachment$indica_bancad)
 impeachment$voto_impeach <- as.factor(impeachment$voto_impeach)
+impeachment$uf <- as.character(impeachment$uf)
+impeachment$partido <- as.character(impeachment$partido)
+impeachment$mun_mais_vota <- as.character(impeachment$mun_mais_vota)
 glimpse(impeachment)
 
 
