4ef2c4fea03a865fb0f109f5ff9235197575961b
Diogo Canut <canut@wcore.com.br>
2018-08-28 16:33:14 -0300

    relatorioaula04

--- dados/script.r
+++ dados/script.r
@@ -1,5 +1,6 @@
 library(dplyr)
 cssv <- read.csv("/home/knut/projetos/MD2018/dados/proposicoes-2018.csv", sep=";")
 cssv <- tbl_df(cssv)
-cssv <- select(cssv,c("id","ementa","ultimoStatus_descricaoTramitacao"))
+cssv <- select(cssv,c("id","ementa","ultimoStatus_descricaoTramitacao", "ultimoStatus_idOrgao"))
 cssv$ultimoStatus_descricaoTramitacao<-as.character(cssv$ultimoStatus_descricaoTramitacao)
+
